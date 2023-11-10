982c9410a248df8c898c61a6508ec687fdff039f
Kim Sang Won <131442242+Teachsue@users.noreply.github.com>
2023-11-10 17:08:09 +0900

    Title: 최댓값 구하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 231110/최댓값 구하기/maximum-value.java
+++ 231110/최댓값 구하기/maximum-value.java
@@ -2,13 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        int max = Math.max(a, Math.max(b, c));
-        System.out.println(max);
+    Scanner sc = new Scanner(System.in);
+    
+    int a, b, c;
+    
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c = sc.nextInt();
+
+    if (a >= b && a >= c) {
+        System.out.println(a);
+        }
+        else {
+            if (b >= a && b >= c) {
+                System.out.println(b);
+            }
+            else System.out.println(c);
+        }
     }
 }
