88dce149b79569139d7cdf8e018c1066ecd14b18
Kim Sang Won <131442242+Teachsue@users.noreply.github.com>
2023-11-21 18:55:07 +0900

    Title: 369 게임, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 231121/369 게임/369-game.java
+++ 231121/369 게임/369-game.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        for (int i = 1; i <= n; i++) {
-            boolean contain369 = (i % 10 == 3) || (i % 10 == 6) || (i % 10 == 9);
-            if (i % 3 == 0 || contain369) {
-                System.out.print("0 ");
-            } else {
-                System.out.print(i + " ");
-            }
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+        System.out.print("0 ");
+    } else {
+        if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            System.out.print("0 ");
+        } else {
+            System.out.print(i + " ");
         }
     }
+}
+}
 }
